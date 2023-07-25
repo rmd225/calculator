2a25bb112e741a9402483ed421ec2b2fdfc6bec9
Riley Dembo <rmd225@lehigh.edu>
2023-07-25 19:40:14 -0400

    blue style change

---
+++ js/index.js
@@ -0,0 +1,19 @@
+function insert(num) {
+    document.form.textview.value = document.form.textview.value + num;
+  }
+  
+  function equal() {
+    var ere = document.form.textview.value;
+    if (ere) {
+      document.form.textview.value = eval(ere);
+    }
+  }
+  
+  function c() {
+    document.form.textview.value = "";
+  }
+  
+  function back() {
+    var ere = document.form.textview.value;
+    document.form.textview.value = ere.substring(0, ere.length - 1);
+  }
